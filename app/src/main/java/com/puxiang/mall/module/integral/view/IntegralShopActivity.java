443a8d0bd1313b5c99ea91e87285a657d9705929
bai <415082375@qq.com>
2017-11-20 08:52:34 +0800

    update on 20171120

---
+++ app/src/main/java/com/puxiang/mall/module/integral/view/IntegralShopActivity.java
@@ -0,0 +1,24 @@
+package com.puxiang.mall.module.integral.view;
+
+import com.puxiang.mall.BaseBindActivity;
+
+/**
+ * Created by zhaoyong bai on 2017/11/18.
+ */
+
+public class IntegralShopActivity extends BaseBindActivity {
+    @Override
+    protected void initBind() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    protected void viewModelDestroy() {
+
+    }
+}
